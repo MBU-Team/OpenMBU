5ce38b95105324ff01a9247318124db7b9db185c
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2021-02-15 15:53:53 -0600

    Started implementing Marble class

--- engine/game/marble/marble.cpp
+++ engine/game/marble/marble.cpp
@@ -5,10 +5,124 @@
 
 #include "game/marble/marble.h"
 
+#include "audio/audioDataBlock.h"
+
 //----------------------------------------------------------------------------
 
 IMPLEMENT_CO_NETOBJECT_V1(Marble);
 
+U32 Marble::smEndPadId = 0;
+
+Marble::Marble()
+{
+    // TODO: Finish Implementation
+    mControllable = true;
+}
+
+Marble::~Marble()
+{
+    // TODO: Implement
+}
+
+void Marble::initPersistFields()
+{
+    Parent::initPersistFields();
+
+    addField("Controllable", TypeBool, Offset(mControllable, Marble));
+}
+
 //----------------------------------------------------------------------------
 
 IMPLEMENT_CO_DATABLOCK_V1(MarbleData);
+
+MarbleData::MarbleData()
+{
+    minVelocityBounceSoft = 2.5f;
+    minVelocityBounceHard = 12.0f;
+    bounceMinGain = 0.2f;
+    minVelocityMegaBounceSoft = 2.5f;
+    maxJumpTicks = 3;
+    bounceEmitter = NULL;
+    minVelocityMegaBounceHard = 8.0f;
+    trailEmitter = NULL;
+    mudEmitter = NULL;
+    bounceMegaMinGain = 0.5f;
+    grassEmitter = NULL;
+    maxRollVelocity = 25.0f;
+    angularAcceleration = 18.0f;
+    brakingAcceleration = 8.0f;
+    gravity = 20.0f;
+    size = 1.0f;
+    megaSize = 2.0f;
+    staticFriction = 1.0f;
+    kineticFriction = 0.89999998f;
+    bounceKineticFriction = 0.2f;
+    maxDotSlide = 0.1f;
+    bounceRestitution = 0.89999998f;
+    airAcceleration = 5.0f;
+    energyRechargeRate = 1.0f;
+    jumpImpulse = 1.0f;
+    maxForceRadius = 1.0f;
+    cameraDistance = 2.5f;
+    minBounceVel = 0.1f;
+    minTrailSpeed = 10.0f;
+    minBounceSpeed = 1.0f;
+    memset(sound, 0, sizeof(sound));
+    genericShadowLevel = 0.40000001f;
+    noShadowLevel = 0.0099999998f;
+    blastRechargeTime = 1;
+    maxNaturalBlastRecharge = 1;
+    powerUps = NULL;
+    mDecalData = NULL;
+    mDecalID = 0;
+    startModeTime = 320;
+    stopModeTime = 32;
+}
+
+void MarbleData::initPersistFields()
+{
+    addField("maxRollVelocity", TypeF32, Offset(maxRollVelocity, MarbleData));
+    addField("angularAcceleration", TypeF32, Offset(angularAcceleration, MarbleData));
+    addField("brakingAcceleration", TypeF32, Offset(brakingAcceleration, MarbleData));
+    addField("staticFriction", TypeF32, Offset(staticFriction, MarbleData));
+    addField("kineticFriction", TypeF32, Offset(kineticFriction, MarbleData));
+    addField("bounceKineticFriction", TypeF32, Offset(bounceKineticFriction, MarbleData));
+    addField("gravity", TypeF32, Offset(gravity, MarbleData));
+    addField("size", TypeF32, Offset(size, MarbleData));
+    addField("megaSize", TypeF32, Offset(megaSize, MarbleData));
+    addField("maxDotSlide", TypeF32, Offset(maxDotSlide, MarbleData));
+    addField("bounceRestitution", TypeF32, Offset(bounceRestitution, MarbleData));
+    addField("airAcceleration", TypeF32, Offset(airAcceleration, MarbleData));
+    addField("energyRechargeRate", TypeF32, Offset(energyRechargeRate, MarbleData));
+    addField("jumpImpulse", TypeF32, Offset(jumpImpulse, MarbleData));
+    addField("maxForceRadius", TypeF32, Offset(maxForceRadius, MarbleData));
+    addField("cameraDistance", TypeF32, Offset(cameraDistance, MarbleData));
+    addField("minBounceVel", TypeF32, Offset(minBounceVel, MarbleData));
+    addField("minTrailSpeed", TypeF32, Offset(minTrailSpeed, MarbleData));
+    addField("minBounceSpeed", TypeF32, Offset(minBounceSpeed, MarbleData));
+    addField("bounceEmitter", TypeParticleEmitterDataPtr, Offset(bounceEmitter, MarbleData));
+    addField("trailEmitter", TypeParticleEmitterDataPtr, Offset(trailEmitter, MarbleData));
+    addField("mudEmitter", TypeParticleEmitterDataPtr, Offset(mudEmitter, MarbleData));
+    addField("grassEmitter", TypeParticleEmitterDataPtr, Offset(grassEmitter, MarbleData));
+    addField("powerUps", TypeGameBaseDataPtr, Offset(powerUps, MarbleData));
+    addField("blastRechargeTime", TypeS32, Offset(blastRechargeTime, MarbleData));
+    addField("maxNaturalBlastRecharge", TypeS32, Offset(maxNaturalBlastRecharge, MarbleData));
+    addField("RollHardSound", TypeAudioProfilePtr, Offset(sound[0], MarbleData));
+    addField("RollMegaSound", TypeAudioProfilePtr, Offset(sound[1], MarbleData));
+    addField("RollIceSound", TypeAudioProfilePtr, Offset(sound[2], MarbleData));
+    addField("SlipSound", TypeAudioProfilePtr, Offset(sound[3], MarbleData));
+    addField("Bounce1", TypeAudioProfilePtr, Offset(sound[4], MarbleData));
+    addField("Bounce2", TypeAudioProfilePtr, Offset(sound[5], MarbleData));
+    addField("Bounce3", TypeAudioProfilePtr, Offset(sound[6], MarbleData));
+    addField("Bounce4", TypeAudioProfilePtr, Offset(sound[7], MarbleData));
+    addField("MegaBounce1", TypeAudioProfilePtr, Offset(sound[8], MarbleData));
+    addField("MegaBounce2", TypeAudioProfilePtr, Offset(sound[9], MarbleData));
+    addField("MegaBounce3", TypeAudioProfilePtr, Offset(sound[10], MarbleData));
+    addField("MegaBounce4", TypeAudioProfilePtr, Offset(sound[11], MarbleData));
+    addField("JumpSound", TypeAudioProfilePtr, Offset(sound[12], MarbleData));
+    Con::addVariable("Game::endPad", TypeS32, &Marble::smEndPadId);
+
+    Parent::initPersistFields();
+}
+
+//----------------------------------------------------------------------------
--- engine/game/marble/marble.h
+++ engine/game/marble/marble.h
@@ -1,3 +1,8 @@
+//-----------------------------------------------------------------------------
+// Torque Shader Engine
+// Copyright (C) GarageGames.com, Inc.
+//-----------------------------------------------------------------------------
+
 #ifndef _MARBLE_H_
 #define _MARBLE_H_
 
@@ -5,13 +10,170 @@
 #include "game/shapeBase.h"
 #endif
 
+#ifndef _DECALMANAGER_H_
+#include "sim/decalManager.h"
+#endif
+
+#ifndef _POWERUP_H_
+#include "game/marble/powerup.h"
+#endif
+
+class MarbleData;
+
 class Marble : public ShapeBase
 {
 private:
     typedef ShapeBase Parent;
 
+    enum MarbleModeFlags
+    {
+        MoveMode = 0x1,
+        RestrictXYZMode = 0x2,
+        CameraHoverMode = 0x4,
+        TimerMode = 0x8,
+        StartingMode = 0x10,
+        StoppingMode = 0x20,
+        FinishMode = 0x40,
+        ActiveModeBits = 0x4,
+        ActiveModeMask = 0xF,
+        ModeBits = 0x7,
+        MaxModeTicks = 0x1F,
+    };
+
+    enum MaskBits
+    {
+        MoveMask = 0x1,
+        RenderModeMask = 0x2,
+    };
+
+    struct Contact
+    {
+        SimObject* object;
+        Point3D position;
+        Point3D normal;
+        Point3F actualNormal;
+        Point3D surfaceVelocity;
+        Point3D surfaceFrictionVelocity;
+        F64 staticFriction;
+        F64 kineticFriction;
+        Point3D vAtC;
+        F64 vAtCMag;
+        F64 normalForce;
+        F64 contactDistance;
+        F32 friction;
+        F32 restitution;
+        F32 force;
+        S32 material;
+    };
+
+    struct SinglePrecision
+    {
+        Point3F mPosition;
+        Point3F mVelocity;
+        Point3F mOmega;
+    };
+
+    struct StateDelta
+    {
+        Point3D pos;
+        Point3D posVec;
+        F32 prevMouseX;
+        F32 prevMouseY;
+        Move move;
+    };
+
+    struct EndPadEffect
+    {
+        F32 effectTime;
+        Point3F lastCamFocus;
+    };
+
+    struct MaterialCollision
+    {
+        U32 ghostIndex;
+        U32 materialId;
+        NetObject* object;
+    };
+
+    struct PowerUpState
+    {
+        bool active;
+        U32 ticksLeft;
+        S32 imageSlot;
+        SimObjectPtr<ParticleEmitter> emitter;
+    };
+
+
+    Marble::SinglePrecision mSinglePrecision;
+    Vector<Marble::Contact> mContacts;
+    Marble::Contact mBestContact;
+    Marble::Contact mLastContact;
+    Point3F mMovePath[2];
+    F32 mMovePathTime[2];
+    U32 mMovePathSize;
+    Marble::StateDelta delta;
+    Point3F mLastRenderPos;
+    Point3F mLastRenderVel;
+    MarbleData* mDataBlock;
+    S32 mBounceEmitDelay;
+    U32 mPowerUpId;
+    U32 mPowerUpTimer;
+    U32 mBlastTimer;
+    U32 mBlastEnergy;
+    F32 mRenderBlastPercent;
+    GFXVertexBufferHandle<GFXVertexP> mVertBuff;
+    GFXPrimitiveBufferHandle mPrimBuff;
+    U32 mMarbleTime;
+    U32 mMarbleBonusTime;
+    U32 mFullMarbleTime;
+    bool mUseFullMarbleTime;
+    U32 mMode;
+    U32 mModeTimer;
+    SimObjectPtr<AudioStreamSource> mRollHandle;
+    SimObjectPtr<AudioStreamSource> mSlipHandle;
+    SimObjectPtr<AudioStreamSource> mMegaHandle;
+    F32 mRadius;
+    QuatF mGravityFrame;
+    QuatF mGravityRenderFrame;
+    Point3D mVelocity;
+    Point3D mPosition;
+    Point3D mOmega;
+    F32 mMouseX;
+    F32 mMouseY;
+    bool mControllable;
+    bool mOOB;
+    Point3F mOOBCamPos;
+    U32 mSuperSpeedDoneTime;
+    F32 mLastYaw;
+    Point3F mNetSmoothPos;
+    bool mCenteringCamera;
+    F32 mRadsLeftToCenter;
+    F32 mRadsStartingToCenter;
+    U32 mCheckPointNumber;
+    Marble::EndPadEffect mEffect;
+    Point3F mLastCamPos;
+    F32 mCameraDist;
+    bool mCameraInit;
+    SceneObject* mPadPtr;
+    bool mOnPad;
+    Marble::PowerUpState mPowerUpState[10];
+    PowerUpData::ActiveParams mPowerUpParams;
+    SimObjectPtr<ParticleEmitter> mTrailEmitter;
+    SimObjectPtr<ParticleEmitter> mMudEmitter;
+    SimObjectPtr<ParticleEmitter> mGrassEmitter;
+    Point3F mShadowPoints[33];
+    bool mShadowGenerated;
+    MatInstance* mStencilMaterial;
+
 public:
     DECLARE_CONOBJECT(Marble);
+
+    Marble();
+    ~Marble();
+
+    static void initPersistFields();
+
+    static U32 smEndPadId;
 };
 
 class MarbleData : public ShapeBaseData
@@ -19,8 +181,57 @@ class MarbleData : public ShapeBaseData
 private:
     typedef ShapeBaseData Parent;
 
+    enum Sounds {
+        SoftImpactSound,
+        HardImpactSound,
+        MaxSounds,
+    };
+
+    AudioProfile* sound[13];
+    F32 maxRollVelocity;
+    F32 minVelocityBounceSoft;
+    F32 minVelocityBounceHard;
+    F32 minVelocityMegaBounceSoft;
+    F32 minVelocityMegaBounceHard;
+    F32 bounceMinGain;
+    F32 bounceMegaMinGain;
+    F32 angularAcceleration;
+    F32 brakingAcceleration;
+    F32 staticFriction;
+    F32 kineticFriction;
+    F32 bounceKineticFriction;
+    F32 gravity;
+    F32 size;
+    F32 megaSize;
+    F32 maxDotSlide;
+    F32 bounceRestitution;
+    F32 airAcceleration;
+    F32 energyRechargeRate;
+    F32 jumpImpulse;
+    F32 cameraDistance;
+    U32 maxJumpTicks;
+    F32 maxForceRadius;
+    F32 minBounceVel;
+    U32 startModeTime;
+    U32 stopModeTime;
+    F32 minBounceSpeed;
+    F32 minTrailSpeed;
+    ParticleEmitterData* bounceEmitter;
+    ParticleEmitterData* trailEmitter;
+    ParticleEmitterData* mudEmitter;
+    ParticleEmitterData* grassEmitter;
+    PowerUpData* powerUps;
+    U32 blastRechargeTime;
+    U32 maxNaturalBlastRecharge;
+    DecalData* mDecalData;
+    S32 mDecalID;
+
 public:
     DECLARE_CONOBJECT(MarbleData);
+
+    MarbleData();
+
+    static void initPersistFields();
 };
 
 #endif // _MARBLE_H_
