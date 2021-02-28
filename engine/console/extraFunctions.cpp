111aaf68712366b2d7d8d1827c18e8715af12fc9
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2021-02-28 15:59:39 -0600

    Get system language for getLanguage ConsoleFunction

--- engine/console/extraFunctions.cpp
+++ engine/console/extraFunctions.cpp
@@ -2,6 +2,8 @@
 #include "console/console.h"
 #include "console/consoleInternal.h"
 
+#include <Windows.h> // TODO: Move to platform specific code
+
 ConsoleFunction(loadZip, void, 2, 2, "(zipName)")
 {
     argc;
@@ -26,7 +28,56 @@ ConsoleFunction(getLanguage, const char*, 1, 1, "()")
 {
     argc;
     char* ret = Con::getReturnBuffer(1024);
-    dSprintf(ret, 1024, "%s", "english");
+
+    // TODO: Move to platform specific code
+    //LCID id = GetUserDefaultLCID();
+    LANGID id = GetUserDefaultUILanguage();
+
+    U8 lang = (U8)id;
+
+    const char* language;
+
+    // Information obtained from:
+    // https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-lcid/70feba9f-294e-491e-b6eb-56532684c37f
+    //
+    // PDF Used (14.0)
+    // https://winprotocoldoc.blob.core.windows.net/productionwindowsarchives/MS-LCID/%5bMS-LCID%5d.pdf
+
+    switch (lang)
+    {
+        case 0x04: // zh
+            language = "chinese";
+            break;
+        case 0x07: // de
+            language = "german";
+            break;
+        case 0x09: // en
+            language = "english";
+            break;
+        case 0x0A: // es
+            language = "spanish";
+            break;
+        case 0x0C: // fr
+            language = "french";
+            break;
+        case 0x10: // it
+            language = "italian";
+            break;
+        case 0x11: // ja
+            language = "japanese";
+            break;
+        case 0x12: // ko
+            language = "korean";
+            break;
+        case 0x16: // pt
+            language = "portuguese";
+            break;
+        default:
+            language = "english";
+            break;
+    }
+
+    dSprintf(ret, 1024, "%s", language);
     return ret;
 }
 
