87da51d79b0cc5df6ee96c180a54296e2238f095
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2021-03-03 15:14:28 -0600

    Implemented Marble::advancePhysics

--- engine/game/marble/marblephysics.cpp
+++ engine/game/marble/marblephysics.cpp
@@ -124,9 +124,139 @@ Point3D Marble::getExternalForces(const Move* move, F64 timeStep)
     return Point3D();
 }
 
-void Marble::advancePhysics(const Move*, U32)
+void Marble::advancePhysics(const Move* move, U32 timeDelta)
 {
-    // TODO: Implement advancePhysics
+    dMemcpy(&delta.posVec, &mPosition, sizeof(delta.posVec));
+
+    smPathItrVec.clear();
+
+    F32 dt = timeDelta / 1000.0;
+
+    Box3F extrudedMarble = this->mWorldBox;
+
+    Point3F thing = (mVelocity * dt) * 1.100000023841858;
+    Point3F absThing = thing.abs();
+
+    extrudedMarble.min += (thing - absThing) * 0.5f;
+    extrudedMarble.max += (thing + absThing) * 0.5f;
+
+    extrudedMarble.min -= dt * 25.0;
+    extrudedMarble.max += dt * 25.0;
+
+    for (PathedInterior* obj = PathedInterior::getPathedInteriors(this); ; obj = obj->getNext())
+    {
+        if (!obj)
+            break;
+
+        if (extrudedMarble.isOverlapped(obj->getExtrudedBox()))
+        {
+            obj->pushTickState();
+            obj->computeNextPathStep(timeDelta);
+            smPathItrVec.push_back(obj);
+        }
+    }
+
+    resetObjectsAndPolys(sContactMask, extrudedMarble);
+
+    bool bouncedYet = false;
+    
+    mMovePathSize = 0;
+    
+    F64 timeRemaining = timeDelta / 1000.0;
+    F64 startTime = timeRemaining;
+    F32 slipAmount = 0.0;
+    F64 contactTime = 0.0;
+
+    U32 it = 0;
+    do
+    {
+        if (timeRemaining == 0.0)
+            break;
+
+        F64 timeStep = 0.00800000037997961;
+        if (timeRemaining < 0.00800000037997961)
+            timeStep = timeRemaining;
+
+        Point3D aControl;
+        Point3D desiredOmega;
+
+        bool isCentered = computeMoveForces(aControl, desiredOmega, move);
+
+        findContacts(sContactMask, 0, 0);
+
+        bool stoppedPaths;
+        velocityCancel(isCentered, 0, bouncedYet, stoppedPaths, smPathItrVec);
+        getExternalForces(move, timeStep);
+
+        Point3D a(0, 0, 0);
+        Point3D A(0, 0, 0);
+        applyContactForces(move, isCentered, aControl, desiredOmega, timeStep, A, a, slipAmount);
+
+        mVelocity += A * timeStep;
+        mOmega += a * timeStep;
+
+        if ((mMode & RestrictXYZMode) != 0)
+            mVelocity.set(0, 0, 0);
+
+        velocityCancel(isCentered, 1, bouncedYet, stoppedPaths, smPathItrVec);
+
+        F64 moveTime = timeStep;
+        computeFirstPlatformIntersect(moveTime, smPathItrVec);
+        testMove(mVelocity, mPosition, moveTime, mRadius, sCollisionMask, 0);
+
+        if (!mMovePathSize && timeStep * 0.99 > moveTime && moveTime > 0.001000000047497451)
+        {
+            F64 diff = startTime - timeRemaining;
+
+            mMovePath[0] = mPosition;
+            mMovePathTime[mMovePathSize] = (diff + moveTime) / startTime;
+
+            mMovePathSize++;
+        }
+
+        F64 currentTimeStep = timeStep;
+        if (timeStep != moveTime)
+        {
+            F64 diff = timeStep - moveTime;
+
+            mVelocity -= A * diff;
+            mOmega -= a * diff;
+
+            currentTimeStep = moveTime;
+        }
+
+        if (this->mContacts.size())
+            contactTime += currentTimeStep;
+
+        timeRemaining -= currentTimeStep;
+
+        timeStep = (startTime - timeRemaining) * 1000.0;
+
+        for (S32 i = 0; i < smPathItrVec.size(); i++)
+        {
+            PathedInterior* pint = smPathItrVec[i];
+            pint->resetTickState(0);
+            pint->advance(timeStep);
+        }
+
+        it++;
+    } while (it <= 10);
+
+    for (S32 i = 0; i < smPathItrVec.size(); i++)
+        smPathItrVec[i]->popTickState();
+
+    F32 contactPct = contactTime * 1000.0 / timeDelta;
+
+    Con::setFloatVariable("testCount", contactPct);
+    Con::setFloatVariable("marblePitch", mMouseY);
+
+    updateRollSound(contactPct, slipAmount);
+
+    dMemcpy(&delta.pos, &mPosition, sizeof(delta.pos));
+
+    delta.posVec -= delta.pos;
+
+    setPosition(mPosition, 0);
 }
 
 ConsoleMethod(Marble, setVelocityRot, bool, 3, 3, "(vel)")
