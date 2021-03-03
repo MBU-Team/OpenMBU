aeebc7f1fb5258ab7798dcdf2291b27d00d29fa4
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2021-03-03 13:01:39 -0600

    Implemented Marble::resetObjectsAndPolys

--- engine/game/marble/marblecollision.cpp
+++ engine/game/marble/marblecollision.cpp
@@ -5,9 +5,12 @@
 
 #include "marble.h"
 
+#include "math/mathUtils.h"
+
 //----------------------------------------------------------------------------
 
 static Box3F sgLastCollisionBox;
+static U32 sgLastCollisionMask;
 static bool sgResetFindObjects;
 static U32 sgCountCalls;
 
@@ -28,7 +31,7 @@ bool Marble::pointWithinPolyZ(const ConcretePolyList::Poly&, const Point3F&, con
     return false;
 }
 
-void Marble::findObjectsAndPolys(U32, const Box3F&, bool)
+void Marble::findObjectsAndPolys(U32 collisionMask, const Box3F& testBox, bool testPIs)
 {
     // TODO: Implement findObjectsAndPolys
 }
@@ -36,6 +39,7 @@ void Marble::findObjectsAndPolys(U32, const Box3F&, bool)
 bool Marble::testMove(Point3D velocity, Point3D& position, F64& deltaT, F64 radius, U32 collisionMask, bool testPIs)
 {
     // TODO: Implement testMove
+
     return false;
 }
 
@@ -49,7 +53,14 @@ void Marble::computeFirstPlatformIntersect(F64&, Vector<PathedInterior*>&)
     // TODO: Implement computeFirstPlatformIntersect
 }
 
-void Marble::resetObjectsAndPolys(U32, const Box3F&)
+void Marble::resetObjectsAndPolys(U32 collisionMask, const Box3F& testBox)
 {
-    // TODO: Implement resetObjectsAndPolys
+    sgLastCollisionBox.min.set(0, 0, 0);
+    sgLastCollisionBox.max.set(0, 0, 0);
+
+    sgResetFindObjects = 1;
+    sgCountCalls = 0;
+
+    if (!smPathItrVec.size())
+        findObjectsAndPolys(collisionMask, testBox, 0);
 }
