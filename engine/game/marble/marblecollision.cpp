0cc068bd21ac62270520746b91b701b2ba4cedf4
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2021-05-15 17:26:43 -0500

    Cleaned up goto statements in Marble::testMove

--- engine/game/marble/marblecollision.cpp
+++ engine/game/marble/marblecollision.cpp
@@ -158,28 +158,29 @@ bool Marble::testMove(Point3D velocity, Point3D& position, F64& deltaT, F64 radi
 
 	Point3D velocityDir = velocity * (1.0 / velLen);
 
-	Point3D deltaVelocity = velocity * deltaT;
-	Point3D finalPosition = position + deltaVelocity;
+	Point3D deltaPosition = velocity * deltaT;
+	Point3D finalPosition = position + deltaPosition;
 
 	// If there is a collision mask
 	if (collisionMask != 0)
 	{
+        // Create a Bounding Box and expand it to include the final position
         Box3F box(mObjBox.min + position - 0.5f, mObjBox.max + position + 0.5f);
 
-		if (deltaVelocity.x >= 0.0)
-            box.max.x += deltaVelocity.x;
+		if (deltaPosition.x >= 0.0)
+            box.max.x += deltaPosition.x;
 		else
-            box.min.x += deltaVelocity.x;
+            box.min.x += deltaPosition.x;
 
-		if (deltaVelocity.y >= 0.0)
-            box.max.y += deltaVelocity.y;
+		if (deltaPosition.y >= 0.0)
+            box.max.y += deltaPosition.y;
 		else
-            box.min.y += deltaVelocity.y;
+            box.min.y += deltaPosition.y;
 
-		if (deltaVelocity.z >= 0.0)
-			box.max.z += deltaVelocity.z;
+		if (deltaPosition.z >= 0.0)
+			box.max.z += deltaPosition.z;
 		else
-            box.min.z += deltaVelocity.z;
+            box.min.z += deltaPosition.z;
 
         findObjectsAndPolys(collisionMask, box, testPIs);
 	}
@@ -195,7 +196,7 @@ bool Marble::testMove(Point3D velocity, Point3D& position, F64& deltaT, F64 radi
 	// Marble on Marble collision
 	if ((collisionMask & PlayerObjectType) != 0)
 	{
-        Point3F nextPos = position + deltaVelocity;
+        Point3F nextPos = position + deltaPosition;
         
         for (S32 i = 0; i < marbles.size(); i++)
         {
@@ -237,18 +238,17 @@ bool Marble::testMove(Point3D velocity, Point3D& position, F64& deltaT, F64 radi
             }  
         }
 	}
-
-	S32 iter = 0;
-	// Marble on Platform collision
-	if (!polyList.mPolyList.empty())
-	{
+    
+    // Marble on Platform collision
+    if (!polyList.mPolyList.empty())
+    {
         Point3D velAddition;
         Point3F vert;
         PlaneD plane;
 
         ConcretePolyList::Poly* poly;
-        S32 index = 0;
-        while(true)
+
+        for (S32 index = 0; index < polyList.mPolyList.size(); index++)
         {
             poly = &polyList.mPolyList[index];
 
@@ -256,214 +256,220 @@ bool Marble::testMove(Point3D velocity, Point3D& position, F64& deltaT, F64 radi
 
             if (plane.y * velocityDir.y + plane.x * velocityDir.x + plane.z * velocityDir.z > -0.001 ||
                 plane.y * finalPosition.y + plane.x * finalPosition.x + plane.z * finalPosition.z + plane.d > radius)
-            {
-                goto CONTINUE_FIRST_LOOP;
-            }
+                continue;
 
             F64 timeVar = (radius - (plane.x * position.x + plane.y * position.y + plane.z * position.z + plane.d))
-                                / (plane.y * velocity.y + plane.x * velocity.x + plane.z * velocity.z);
+                / (plane.y * velocity.y + plane.x * velocity.x + plane.z * velocity.z);
 
-            if (timeVar < 0.0 || finalT < timeVar)
-                break;
-
-            U32 vertIndex = polyList.mIndexList[poly->vertexCount - 1 + poly->vertexStart];
-            vert = polyList.mVertexList[vertIndex];
-
-            velAddition = velocity * timeVar + position;
-
-            bool hasVertices = poly->vertexCount != 0;
-
-            if (hasVertices)
+            if (!(timeVar < 0.0 || finalT < timeVar))
             {
-                U32 i = 0;
-                do
+                U32 vertIndex = polyList.mIndexList[poly->vertexCount - 1 + poly->vertexStart];
+                vert = polyList.mVertexList[vertIndex];
+
+                velAddition = velocity * timeVar + position;
+
+                bool hasVertices = poly->vertexCount != 0;
+
+                if (hasVertices)
                 {
-                    Point3F newVert = polyList.mVertexList[polyList.mIndexList[i + poly->vertexStart]];
-                    if (newVert != vert)
+                    U32 i = 0;
+                    do
                     {
-                        PlaneD pl(Point3D(newVert + plane), newVert, vert);
-                        vert = newVert;
-                        if (pl.x * velAddition.x + pl.y * velAddition.y + pl.z * velAddition.z + pl.d < 0.0)
-                            break;
-                    }
+                        Point3F newVert = polyList.mVertexList[polyList.mIndexList[i + poly->vertexStart]];
+                        if (newVert != vert)
+                        {
+                            PlaneD pl(Point3D(newVert + plane), newVert, vert);
+                            vert = newVert;
+                            if (pl.x * velAddition.x + pl.y * velAddition.y + pl.z * velAddition.z + pl.d < 0.0)
+                                break;
+                        }
 
-                    i++;
-                } while(i < poly->vertexCount);
+                        i++;
+                    } while (i < poly->vertexCount);
 
-                hasVertices = i != poly->vertexCount;
-            }
+                    hasVertices = i != poly->vertexCount;
+                }
 
-            if (hasVertices)
-                break;
-
-            finalT = timeVar;
-            finalPosition = velAddition;
-            lastContactPos = plane.project(velAddition);
-            contactPoly = poly;
-
-CONTINUE_FIRST_LOOP:
-            index++;
-            if (index >= polyList.mPolyList.size())
-                goto superbreak;
-        }
-
-        Point3F nextVert = polyList.mVertexList[polyList.mIndexList[poly->vertexCount - 1 + poly->vertexStart]];
-
-        if (poly->vertexCount == 0)
-            goto CONTINUE_FIRST_LOOP;
-
-        F64 lastZ;
-
-        Point3D theVert;
-        Point3D velTimePosVert;
-        Point3D thePosThing;
-        Point3D theVelocity;
-        Point3D vertDiff;
-        Point3D posDiff;
-
-        F64 radSq = radius * radius;
-        S32 iter = 0;
-        while (true)
-        {
-            theVert = polyList.mVertexList[polyList.mIndexList[iter + poly->vertexStart]];
-
-            vertDiff = nextVert - theVert;
-
-            posDiff = position - theVert;
-
-            mCross(vertDiff, velocity, &theVelocity);
-
-            mCross(vertDiff, posDiff, &thePosThing);
-
-            F64 theVelLen = theVelocity.lenSquared();
-            F64 dotVel = mDot(thePosThing, theVelocity);
-            F64 dotVelSq = dotVel + dotVel;
-
-            F64 someNum = dotVelSq * dotVelSq - (thePosThing.lenSquared() - vertDiff.lenSquared() * radSq)
-                        * (theVelLen * 4.0);
-
-            if (theVelLen == 0.0 || someNum < 0.0)
-                goto noedgeint;
-
-            F64 what = 0.5 / theVelLen;
-            F64 someNumSqrt = mSqrtD(someNum);
-
-            F64 t1 = (someNumSqrt - dotVelSq) * what;
-            F64 t2 = (-dotVelSq - someNumSqrt) * what;
-            if (t2 < t1)
-            {
-                F64 temp = t2;
-                t2 = t1;
-                t1 = temp;
-            }
-
-            if (t2 <= 0.0001 || finalT <= t1)
-                goto noedgeint;
-                
-            if (t1 < 0.0)
-                goto LABEL_52;
-
-            F64 vertDiffLen = vertDiff.len();
-            
-            velTimePosVert = velocity * t1 + position - theVert;
-
-            F64 theVar = mDot(velTimePosVert, vertDiff) / vertDiffLen;
-
-            if (-radius > theVar || vertDiffLen + radius < theVar)
-                goto noedgeint;
-
-            if (theVar < 0.0)
-                break;
-            if (theVar > vertDiffLen)
-                goto LABEL_52;
-
-            finalT = t1;
-            finalPosition = velocity * t1 + position;
-
-            lastContactPos = vertDiff * (theVar / vertDiffLen) + theVert;
-            contactPoly = poly;
-
-noedgeint:
-            bool notGoBack = ++iter < poly->vertexCount;
-
-            nextVert = theVert;
-
-            if (!notGoBack)
-                goto CONTINUE_FIRST_LOOP;
-        }
-LABEL_52:
-        F64 wow = velocity.lenSquared();
-
-	    Point3D posVertDiff = position - theVert;
-        F64 posVertDiffDot = mDot(posVertDiff, velocity);
-        F64 posVertDiffDotSq = posVertDiffDot + posVertDiffDot;
-
-        F64 tx = wow * 4.0;
-
-        F64 var12 = posVertDiffDotSq * posVertDiffDotSq - (posVertDiff.lenSquared() - radSq) * tx;
-
-        if (wow != 0.0 && var12 >= 0.0)
-        {
-            F64 var13 = 0.5 / wow;
-            F64 var12Sqrt = mSqrtD(var12);
-            F64 var14 = -posVertDiffDotSq - var12Sqrt;
-            F64 var1 = (var12Sqrt - posVertDiffDotSq) * var13;
-            F64 var2 = var14 * var13;
-            if (var2 < var1)
-            {
-                F64 temp = var2;
-                var2 = var1;
-                var1 = temp;
-            }
-            if (var2 > 0.0001 && finalT > var1)
-            {
-                if (var1 <= 0.0 && var1 > -0.0001)
-                    var1 = 0.0;
-
-                if (var1 >= 0.0)
+                if (!hasVertices)
                 {
-                    finalT = var1;
+                    finalT = timeVar;
+                    finalPosition = velAddition;
+                    lastContactPos = plane.project(velAddition);
                     contactPoly = poly;
-                    finalPosition = velocity * var1 + position;
-                    lastContactPos = theVert;
+                    continue;
                 }
             }
+
+            Point3F nextVert = polyList.mVertexList[polyList.mIndexList[poly->vertexCount - 1 + poly->vertexStart]];
+
+            if (poly->vertexCount == 0)
+                continue;
+
+            F64 lastZ;
+
+            Point3D theVert;
+            Point3D velTimePosVert;
+            Point3D thePosThing;
+            Point3D theVelocity;
+            Point3D vertDiff;
+            Point3D posDiff;
+
+            F64 radSq = radius * radius;
+
+            for (S32 iter = 0; iter < poly->vertexCount; iter++)
+            {
+                theVert = polyList.mVertexList[polyList.mIndexList[iter + poly->vertexStart]];
+
+                vertDiff = nextVert - theVert;
+
+                posDiff = position - theVert;
+
+                mCross(vertDiff, velocity, &theVelocity);
+
+                mCross(vertDiff, posDiff, &thePosThing);
+
+                F64 theVelLen = theVelocity.lenSquared();
+                F64 dotVel = mDot(thePosThing, theVelocity);
+                F64 dotVelSq = dotVel + dotVel;
+
+                F64 someNum = dotVelSq * dotVelSq - (thePosThing.lenSquared() - vertDiff.lenSquared() * radSq)
+                    * (theVelLen * 4.0);
+
+                if (theVelLen == 0.0 || someNum < 0.0)
+                {
+                    nextVert = theVert;
+                    continue;
+                }
+
+                F64 what = 0.5 / theVelLen;
+                F64 someNumSqrt = mSqrtD(someNum);
+
+                F64 t1 = (someNumSqrt - dotVelSq) * what;
+                F64 t2 = (-dotVelSq - someNumSqrt) * what;
+                if (t2 < t1)
+                {
+                    F64 temp = t2;
+                    t2 = t1;
+                    t1 = temp;
+                }
+
+                if (t2 <= 0.0001 || finalT <= t1)
+                {
+                    nextVert = theVert;
+                    continue;
+                }
+                if (!(t1 < 0.0))
+                {
+                    F64 vertDiffLen = vertDiff.len();
+
+                    velTimePosVert = velocity * t1 + position - theVert;
+
+                    F64 theVar = mDot(velTimePosVert, vertDiff) / vertDiffLen;
+
+                    if (-radius > theVar || vertDiffLen + radius < theVar)
+                    {
+                        nextVert = theVert;
+                        continue;
+                    }
+                    if (!(theVar < 0.0))
+                    {
+                        if (!(theVar > vertDiffLen))
+                        {
+                            finalT = t1;
+                            finalPosition = velocity * t1 + position;
+
+                            lastContactPos = vertDiff * (theVar / vertDiffLen) + theVert;
+                            contactPoly = poly;
+
+                            nextVert = theVert;
+                            continue;
+                        }
+                    }
+                }
+
+                F64 wow = velocity.lenSquared();
+
+                Point3D posVertDiff = position - theVert;
+                F64 posVertDiffDot = mDot(posVertDiff, velocity);
+                F64 posVertDiffDotSq = posVertDiffDot + posVertDiffDot;
+
+                F64 tx = wow * 4.0;
+
+                F64 var12 = posVertDiffDotSq * posVertDiffDotSq - (posVertDiff.lenSquared() - radSq) * tx;
+
+                if (wow != 0.0 && var12 >= 0.0)
+                {
+                    F64 var13 = 0.5 / wow;
+                    F64 var12Sqrt = mSqrtD(var12);
+                    F64 var14 = -posVertDiffDotSq - var12Sqrt;
+                    F64 var1 = (var12Sqrt - posVertDiffDotSq) * var13;
+                    F64 var2 = var14 * var13;
+                    if (var2 < var1)
+                    {
+                        F64 temp = var2;
+                        var2 = var1;
+                        var1 = temp;
+                    }
+                    if (var2 > 0.0001 && finalT > var1)
+                    {
+                        if (var1 <= 0.0 && var1 > -0.0001)
+                            var1 = 0.0;
+
+                        if (var1 >= 0.0)
+                        {
+                            finalT = var1;
+                            contactPoly = poly;
+                            finalPosition = velocity * var1 + position;
+                            lastContactPos = theVert;
+                        }
+                    }
+                }
+
+                Point3D var3 = position - nextVert;
+                F64 var4 = mDot(var3, velocity);
+                F64 var5 = var4 + var4;
+                F64 var6 = var5 * var5 - (var3.lenSquared() - radSq) * tx;
+
+                if (wow == 0.0 || var6 < 0.0)
+                {
+                    nextVert = theVert;
+                    continue;
+                }
+
+                F64 var7 = 0.5 / wow;
+                F64 var8 = mSqrtD(var6);
+                F64 var9 = -var5 - var8;
+                F64 var10 = (var8 - var5) * var7;
+                F64 var11 = var9 * var7;
+                if (var11 < var10)
+                {
+                    F64 temp = var11;
+                    var11 = var10;
+                    var10 = temp;
+                }
+
+                if (var11 <= 0.0001 || finalT <= var10)
+                {
+                    nextVert = theVert;
+                    continue;
+                }
+                if (var10 <= 0.0 && var10 > -0.0001)
+                    var10 = 0.0;
+                if (var10 < 0.0)
+                {
+                    nextVert = theVert;
+                    continue;
+                }
+
+                finalT = var10;
+                finalPosition = velocity * var10 + position;
+                lastContactPos = nextVert;
+                contactPoly = poly;
+
+                nextVert = theVert;
+            }
         }
-
-        Point3D var3 = position - nextVert;
-        F64 var4 = mDot(var3, velocity);
-        F64 var5 = var4 + var4;
-        F64 var6 = var5 * var5 - (var3.lenSquared() - radSq) * tx;
-        if (wow == 0.0 || var6 < 0.0)
-            goto noedgeint;
-
-        F64 var7 = 0.5 / wow;
-        F64 var8 = mSqrtD(var6);
-        F64 var9 = -var5 - var8;
-        F64 var10 = (var8 - var5) * var7;
-        F64 var11 = var9 * var7;
-        if (var11 < var10)
-        {
-            F64 temp = var11;
-            var11 = var10;
-            var10 = temp;
-        }
-
-        if (var11 <= 0.0001 || finalT <= var10)
-            goto noedgeint;
-        if (var10 <= 0.0 && var10 > -0.0001)
-            var10 = 0.0;
-        if (var10 < 0.0)
-            goto noedgeint;
-
-        finalT = var10;
-        finalPosition = velocity * var10 + position;
-        lastContactPos = nextVert;
-        contactPoly = poly;
-
-        goto noedgeint;
-	}
-superbreak:
+    }
 
     position = finalPosition;
 
