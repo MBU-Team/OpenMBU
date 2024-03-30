b3afd463daa2df14631527ba6f1b83da9507e8c0
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2024-03-30 17:35:10 -0500

    Upped version number

--- game/main.cs
+++ game/main.cs
@@ -377,7 +377,7 @@ function initVideo()
          $pref::Video::FSAALevel = 0;
    }
 
-   $canvasCreated = createCanvas("Marble Blast Ultra! - 1.17.2");
+   $canvasCreated = createCanvas("Marble Blast Ultra! - 1.17.3");
    
    // We need to set up Gamma Ramp here so that the splash screens are affected.
    
